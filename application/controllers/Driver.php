5dafc6785f70a470f68584f346133e4180897f72
rohit <rohit.vish@darwinpgc.com>
2021-03-22 17:49:17 +0530

    Driver

--- application/controllers/Driver.php
+++ application/controllers/Driver.php
@@ -53,20 +53,20 @@ class Driver extends API_Controller {
 		$phone = $this->input->post('phone');
 		if(!$phone){
 			echo json_encode([
-				"status" => "1",
+				"status" => true,
 				"message" => "fields not provided"
 			]);exit;
 		}
 		if(strlen($phone) != 10){
 			echo json_encode([
-				"status" => "1",
+				"status" => true,
 				"message" => "Invalid mobile number"
 			]);exit;
 		}
 		$data = $this->db->select('status')->from('user_register')->where('phone', $phone)->get()->result_array();
 		if($data[0]['status']!='1'){
 			echo json_encode([
-				"status" => '0',
+				"status" => false,
 				"message" => "Account is disabled"
 			]);exit;
 		}
@@ -82,7 +82,7 @@ class Driver extends API_Controller {
 			$str1=explode('|',$response2);
             $str= str_replace(' ','',$str1[0]);
 			if($str=='success'){
-                // response
+				$this->db->set('active', 'login')->where('phone', $phone)->update('driver_register');
 				echo json_encode([
 					"token" => encrypt($query[0]['id']),
 					"OTP" => $otp,
@@ -91,13 +91,13 @@ class Driver extends API_Controller {
 				]);
             }else{
 				echo json_encode([
-					"status" => "0",
+					"status" => false,
 					"message" => "Server error"
 				]);exit;
 			}
 		}else{
 			echo json_encode([
-				"status" => '1',
+				"status" => false,
 				"message" => "Account does not exist"
 			]);exit;
 		}
@@ -108,7 +108,7 @@ class Driver extends API_Controller {
 	{
 		if(!$id){
 			echo json_encode([
-				"status" => "0",
+				"status" => false,
 				"message" => "Invalid parameter"
 			]);exit;
 		}
@@ -120,12 +120,12 @@ class Driver extends API_Controller {
 		$query = $this->db->get()->result_array();
 		if(count($query) == 1){
 			echo json_encode([
-				"status" => "1",
+				"status" => true,
 				"data" => $query[0]
 			]);
 		} else {
 			echo json_encode([
-				"status" => "0",
+				"status" => false,
 				"message" => "No data found"
 			]);exit;
 		}
@@ -141,13 +141,13 @@ class Driver extends API_Controller {
 		if(count($query) > 1){
 			foreach($query as $data){
 				echo json_encode([
-					"status" => "1",
+					"status" => true,
 					"data" => $data
 				]);
 			}
 		}else{
 			echo json_encode([
-				"status" => '1',
+				"status" => false,
 				"message" => "No data found"
 			]);exit;
 		}
@@ -160,13 +160,13 @@ class Driver extends API_Controller {
 		$booking_type=$this->input->post('booking_type');
 		if(!$id){
 			echo json_encode([
-				"status" => "0",
+				"status" => false,
 				"message" => "Invalid parameter"
 			]);exit;
 		}
 		if(!$booking_type){
 			echo json_encode([
-				"status" => "0",
+				"status" => false,
 				"message" => "No booking found"
 			]);exit;
 		}
@@ -181,13 +181,13 @@ class Driver extends API_Controller {
 			if(count($query) > 0){
 				foreach($query as $data){
 					echo json_encode([
-						"status" => "1",
+						"status" => true,
 						"data" => $data
 					]);
 				}
 			}else{
 				echo json_encode([
-					"status" => "0",
+					"status" => false,
 					"message" => "No booking found"
 				]);exit;
 			}
@@ -203,13 +203,13 @@ class Driver extends API_Controller {
 			if(count($query) > 0){
 				foreach($query as $row){
 					echo json_encode([
-						"status" => "1",
+						"status" => true,
 						"data" => $row
 					]);
 				}
 			}else{
 				echo json_encode([
-					"status" => "0",
+					"status" => false,
 					"message" => "No data found"
 				]);exit;
 			}
@@ -225,13 +225,13 @@ class Driver extends API_Controller {
 			if(count($query) > 0){
 				foreach($query as $row){
 					echo json_encode([
-						"status" => "1",
+						"status" => true,
 						"data" => $row
 					]);
 				}
 			}else{
 				echo json_encode([
-					"status" => "0",
+					"status" => false,
 					"message" => "No data found"
 				]);exit;
 			}
@@ -244,17 +244,16 @@ class Driver extends API_Controller {
 			$this->db->where('booking_details.driver_id', $id);
 			$this->db->where('booking_details.status', '1');
 			$query = $this->db->get()->result_array();
-			// print_r($this->db->last_query());exit;
 			if(count($query) > 0){
 				foreach($query as $row){
 					echo json_encode([
-						"status" => "1",
+						"status" => true,
 						"data" => $row
 					]);
 				}
 			}else{
 				echo json_encode([
-					"status" => "0",
+					"status" => false,
 					"message" => "No data found"
 				]);exit;
 			}
@@ -267,17 +266,16 @@ class Driver extends API_Controller {
 			$this->db->where('booking_details.driver_id', $id);
 			$this->db->where('booking_details.status', '1');
 			$query = $this->db->get()->result_array();
-			// print_r($this->db->last_query());exit;
 			if(count($query) > 0){
 				foreach($query as $row){
 					echo json_encode([
-						"status" => "1",
+						"status" => true,
 						"data" => $row
 					]);
 				}
 			}else{
 				echo json_encode([
-					"status" => "0",
+					"status" => false,
 					"message" => "No data found"
 				]);exit;
 			}
@@ -294,13 +292,13 @@ class Driver extends API_Controller {
 			if(count($query) > 0){
 				foreach($query as $row){
 					echo json_encode([
-						"status" => "1",
+						"status" => true,
 						"data" => $row
 					]);
 				}
 			}else{
 				echo json_encode([
-					"status" => "0",
+					"status" => false,
 					"message" => "No data found"
 				]);exit;
 			}
@@ -316,20 +314,379 @@ class Driver extends API_Controller {
 			if(count($query) > 0){
 				foreach($query as $row){
 					echo json_encode([
-						"status" => "1",
+						"status" => true,
 						"data" => $row
 					]);
 				}
 			}else{
 				echo json_encode([
-					"status" => "0",
+					"status" => false,
 					"message" => "No data found"
 				]);exit;
 			}
 		}
 		if($booking_type=='count_dashboard'){
-			$new = $this->db->select('COUNT(ride_status) as new')->from('booking_details')->where('ride_status', 'new')->where('driver_id', $id)->get()->result_array();
-			echo $this->db->last_query();exit;
+			$new = $this->db->select('COUNT(ride_status) as new')->from('booking_details')->where('ride_status', 'new')->where('driver_id', $id)->get()->result_array(); 
+			$data['new'] = $new[0]['new'];
+			$confirm = $this->db->select('COUNT(ride_status) as confirm')->from('booking_details')->where('ride_status', 'confirm')->where('driver_id', $id)->get()->result_array();
+			$data['confirm'] = $confirm[0]['confirm'];
+			$arrived = $this->db->select('COUNT(ride_status) as arrived')->from('booking_details')->where('ride_status', 'arrived')->where('driver_id', $id)->get()->result_array();
+			$data['arrived'] = $arrived[0]['arrived'];
+			$onride = $this->db->select('COUNT(ride_status) as onride')->from('booking_details')->where('ride_status', 'onride')->where('driver_id', $id)->get()->result_array();
+			$data['onride'] = $onride[0]['onride'];
+			$completed = $this->db->select('COUNT(ride_status) as completed')->from('booking_details')->where('ride_status', 'completed')->where('driver_id', $id)->get()->result_array();
+			$data['completed'] = $completed[0]['completed'];
+			$cancel = $this->db->select('COUNT(ride_status) as cancel')->from('booking_details')->where('ride_status', 'cancel')->where('driver_id', $id)->get()->result_array();
+			$data['cancel'] = $cancel[0]['cancel'];
+			echo json_encode([
+				"status" => true,
+				"data" => $data
+			]);
+		}
+	}
+
+	// purchase subscription
+	public function purchase_sub()
+	{
+		$id = $this->input->post('id');
+		$sub_type = $this->input->post('sub_type');
+		if(!$id || !$sub_type){
+			echo json_encode([
+				"status" => false,
+				"message" => "Fields not provided"
+			]);exit;
+		}
+		$date = $this->db->select('sub_exp_date')->from('driver_register')->where('id', $id)->get()->result_array();
+		if($sub_type=='1'){
+			$d = strtotime("+1 month",strtotime($date[0]['sub_exp_date']));
+			$sub_exp_date = date("Y-m-d",$d);
+			$sql = $this->db->set('sub_exp_date', $sub_exp_date)->where('id', $id)->update('driver_register');
+			if($sql){
+				echo json_encode([
+					"status" => true,
+					"message" => "success"
+				]);
+			}else{
+				echo json_encode([
+					"status" => false,
+					"message" => $this->db->_error_message()
+				]);exit;
+			}
+		}
+		if($sub_type=='3'){
+			$d = strtotime("+3 month",strtotime($date[0]['sub_exp_date']));
+			$sub_exp_date = date("Y-m-d",$d);
+			$sql = $this->db->set('sub_exp_date', $sub_exp_date)->where('id', $id)->update('driver_register');
+			if($sql){
+				echo json_encode([
+					"status" => true,
+					"message" => "success"
+				]);
+			}else{
+				echo json_encode([
+					"status" => false,
+					"message" => $this->db->_error_message()
+				]);exit;
+			}
+		}
+		if($sub_type=='6'){
+			$d = strtotime("+6 month",strtotime($date[0]['sub_exp_date']));
+			$sub_exp_date = date("Y-m-d",$d);
+			$sql = $this->db->set('sub_exp_date', $sub_exp_date)->where('id', $id)->update('driver_register');
+			if($sql){
+				echo json_encode([
+					"status" => true,
+					"message" => "success"
+				]);
+			}else{
+				echo json_encode([
+					"status" => false,
+					"message" => $this->db->_error_message()
+				]);exit;
+			}
+		}
+		if($sub_type=='12'){
+			$d = strtotime("+12 month",strtotime($date[0]['sub_exp_date']));
+			$sub_exp_date = date("Y-m-d",$d);
+			$sql = $this->db->set('sub_exp_date', $sub_exp_date)->where('id', $id)->update('driver_register');
+			if($sql){
+				echo json_encode([
+					"status" => true,
+					"message" => "success"
+				]);
+			}else{
+				echo json_encode([
+					"status" => false,
+					"message" => $this->db->_error_message()
+				]);exit;
+			}
+		}
+	}
+
+	// service list
+	public function service_list()
+	{
+		$data = $this->db->select('*')->from('service_city')->order_by('id', 'desc')->get()->result_array();
+		if(count($data) > 0){
+			foreach ($data as $row) {
+				echo json_encode([
+					"status" => true,
+					"data" => $row
+				]);
+			}
+		}else{
+			echo json_encode([
+				"status" => false,
+				"message" => "No data found"
+			]);
+		}	
+	}
+
+	// report user
+	public function report_user() 
+	{
+		$driver_id = $this->input->post('reporter_id');
+		$user_id = $this->input->post('reported_id');
+		$desc = $this->input->post('description');
+		$query = $this->db->select('id')->from('ud_report')->where('reporter', '2')->where('reporter', $driver_id)->where('reported_id', $user_id)->get()->result_array();
+		if($query[0] != 0){
+			echo json_encode([
+				"status" => false,
+				"message" => "Already reported to this driver!"
+			]);exit;
+		}
+		if(!$driver_id || !$user_id || !$desc){
+			echo json_encode([
+				"status" => false,
+				"message" => "fields not provided"
+			]);exit;
+		}
+		$data = array(
+			"reporter" => "2",
+			"reporter_id" => $driver_id,
+			"reported_id" => $user_id,
+			"description" => $desc
+		);
+		$this->db->insert('ud_report', $data);
+		if($this->db->affected_rows() > 0){
+			echo json_encode([
+				"status" => true,
+				"message" => "Successfully reported"
+			]);
+		}else{
+			echo json_encode([
+				"status" => false,
+				"message" => "Server error"
+			]);
+		}
+	}
+
+	// subscription
+	public function subscription() 
+	{
+		$id = $this->input->post('id');
+		if(!$id){
+			echo json_encode([
+				"status" => false,
+				"message" => "invalid parameter"
+			]);exit;
+		}
+		$sql = $this->db->select('*')->from('subscription')->where('type', 'Driver')->get()->result_array();
+		$sql2 = $this->db->select('sub_type,sub_exp_date')->from('driver_register')->where('id', $id)->get()->result_array();
+		foreach($sql as $row){
+				$response["sub_type"] = $sql2[0]['sub_type'];
+				$response["sub_exp_date"] = $sql2[0]['sub_exp_date'];
+				$response["status"] = true;
+				$response["data"][] = $row;
+		}
+		echo json_encode($response);
+	}
+
+	// update driver status
+	public function update_driver_status()
+	{
+		$id = $this->input->post('id');
+		$type = $this->input->post('type');
+		$value = $this->input->post('value');
+		if(!$id || !$type || !$value){
+			echo json_encode([
+				"status" => false,
+				"message" => "fields not provided"
+			]);exit;
+		}
+		if($type=='online'){
+			$msg = $value == 'yes' ? 'Online':'Offline';
+			$sql = $this->db->set('online', $value)->where('id', $id)->update('driver_register');
+			if($sql){
+				echo json_encode([
+					"status" => true,
+					"message" => "You are ".$msg." now"
+				]);
+			}else{
+				echo json_encode([
+					"status" => false,
+					"message" => "Server error"
+				]);exit;
+			}
+		}
+		if($type=='gohome'){
+			$msg = $value == '1' ? 'activated':'deactivated';
+			$sql = $this->db->set('go_home', $value)->set('go_home_last_time', date('Y-m-d H:i:s'))->where('id', $id)->update('driver_register');
+			if($sql){
+				echo json_encode([
+					"status" => true,
+					"message" => "Go home ".$msg." successfully"
+				]);
+			}else{
+				echo json_encode([
+					"status" => false,
+					"message" => "Server error"
+				]);exit;
+			}
+		}
+	}
+
+	// update location
+	public function update_location()
+	{
+		$id = $this->input->post('id');
+		$lat = $this->input->post('lat');
+		$long = $this->input->post('long');	
+		if(!$id || !$lat || !$long){
+			echo json_encode([
+				"status" => false,	
+				"message" => "fields not provided"
+			]);exit;
+		}	
+		$sql = $this->db->set('latitude', $lat)->set('longitude', $long)->where('id', $id)->update('driver_register');
+		if($sql){
+			echo json_encode([
+				"status" => true,
+				"message" => "location updated successfully"
+			]);
+		}else{
+			echo json_encode([
+				"status" => true,
+				"message" => "Driver on same place"
+			]);exit;
+		}
+	}
+
+	// rental price
+	public function rental_price()
+	{
+		$sql = $this->db->select('*')->from('rental_price')->get()->result_array();
+		$i = 0;
+		foreach($sql as $row){
+			$response['status']=true;
+			$response[$i]['id'] = $row['id'];
+			$response[$i]['vehicle_id'] = $row['vehicle_id'];
+			$response[$i]['km']['10'] = $row[10];
+			$response[$i]['km']['20'] = $row[20];
+			$response[$i]['km']['30'] = $row[30];
+			$response[$i]['km']['40'] = $row[40];
+			$response[$i]['km']['60'] = $row[60];
+			$response[$i]['km']['80'] = $row[80];
+			$response[$i]['km']['100'] = $row[100];
+			$response[$i]['km']['120'] = $row[120];
+			$i++;
+		}
+		if(empty($response)){
+			$response['status']=false;
+		}
+		echo json_encode($response);
+	}
+
+	// fetch alert
+	public function fetch_alert()
+	{
+		$sql = $this->db->select('*')->from('alert')->where('status', 1)->get()->result_array();
+		if(count($sql) > 0){
+			foreach ($sql as $row) {
+				echo json_encode([
+					"status" => true,
+					"data" => $row
+				]); 
+			}
+		}else{
+			echo json_encode([
+				"status" => false,
+				"message" => "No data found"
+			]);exit;
+		}
+	}
+
+	// logout driver
+	public function logout()
+	{
+		$phone = $this->input->post('phone');
+		if(!$phone){
+			echo json_encode([
+				"status" => false,
+				"message" => "fields not provided"
+			]);exit;
+		}
+		$sql = $this->db->set('active', 'logout')->where('phone', $phone)->update('driver_register');
+		if($sql){
+			echo json_encode([
+				"status" => true,
+				"message" => "logout successful" 
+			]);
+		}else{
+			echo json_encode([
+				"status" => false,
+				"message" => "server error" 
+			]);exit;
+		}
+	}
+
+	// driver transaction history
+	public function driver_transaction()
+	{
+		$driver_id = $this->input->post('driver_id');
+		if(!$driver_id){
+			echo json_encode([
+				"status" => false,
+				"message" => "fields not provided"
+			]);exit;
+		}
+		$sql = $this->db->select('*')->from ('driver_transaction')->where('driver_id', $driver_id)->order_by('id', 'desc')->get()->result_array();
+		if(count($sql) > 0){
+			foreach ($sql as $row) {
+				echo json_encode([
+					"status" => true,
+					"data" => $row
+				]);
+			}
+		}else{
+			echo json_encode([
+				"status" => false,
+				"message" => "No data found"
+			]);exit;
+		}
+	}
+
+	// get wallet
+	public function get_wallet()
+	{
+		$id = $this->input->post('id');
+		if(!$id){
+			echo json_encode([
+				"status" => false,
+				"message" => "fields not provided"
+			]);exit;
+		}
+		$sql = $this->db->select('wallet')->from('driver_register')->where('id', $id)->get()->result_array();
+		if(count($sql) > 0){
+			echo json_encode([
+				"status" => true,
+				"data" => $sql[0]['wallet']
+			]);
+		}else{
+			echo json_encode([
+				"status" => true,
+				"message" => "server error"
+			]);exit;
 		}
 	}
 }
